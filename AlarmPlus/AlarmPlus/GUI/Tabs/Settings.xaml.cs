805f04a2275d9fb63d5f840c5932685d43a6dac3
louay-hesham <louay.hesham@gmail.com>
2017-03-19 17:37:15 +0200

    Project renaming

---
+++ AlarmPlus/AlarmPlus/GUI/Tabs/Settings.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AlarmPlus.GUI.Tabs
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Settings : ContentPage
+    {
+        public Settings()
+        {
+            InitializeComponent();
+        }
+    }
+}
