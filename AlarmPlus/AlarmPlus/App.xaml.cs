a4d81fe39d7bc7d6646eeb5939b4d1edfe389ee1
louay-hesham <louay.hesham@gmail.com>
2017-03-25 20:05:12 +0200

    Added JsonConvert Package and PCL Storage Package

--- AlarmPlus/AlarmPlus/App.xaml.cs
+++ AlarmPlus/AlarmPlus/App.xaml.cs
@@ -1,47 +1,66 @@
 ﻿using AlarmPlus.Core;
+using Newtonsoft.Json;
+using PCLStorage;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using System.Threading.Tasks;
 using Xamarin.Forms;
 
 namespace AlarmPlus
 {
     public partial class App : Application
-    { 
+    {
+
+        public static async Task SaveAlarms()
+        {
+            IFolder rootFolder = FileSystem.Current.LocalStorage;
+            IFile file = await rootFolder.CreateFileAsync("Alarms", CreationCollisionOption.ReplaceExisting);
+            await file.WriteAllTextAsync(JsonConvert.SerializeObject(Alarm.Alarms));
+        }
+
+        public static async Task LoadAlarms()
+        {
+            IFolder rootFolder = FileSystem.Current.LocalStorage;
+            var x = await rootFolder.CheckExistsAsync("Alarms");
+
+            if (x.Equals(PCLStorage.ExistenceCheckResult.FileExists))
+            {
+                IFile file = await rootFolder.GetFileAsync("Alarms");
+                Alarm.Alarms.Clear();
+                if (file != null)
+                {
+                    string serializedAlarms = await file.ReadAllTextAsync();
+                    if (serializedAlarms != null && !serializedAlarms.Equals(string.Empty))
+                    {
+                        Alarm.Alarms.AddRange(JsonConvert.DeserializeObject<List<Alarm>>(serializedAlarms));
+                    }
+                }
+            }
+            
+        }
+
         public App()
         {
+            LoadAlarms();
             InitializeComponent();
-
             MainPage = new NavigationPage( new AlarmPlus.GUI.MainTabbedPage() );
         }
 
-        protected override void OnStart()
+        protected async override void OnStart()
         {
-            // Handle when your app starts
-            if (Application.Current.Properties.ContainsKey("Alarms"))
-            {
-                var alarms = Current.Properties["Alarms"] as List<Alarm>;
-                Alarm.Alarms.AddRange(alarms);
-            }
+            await LoadAlarms();
         }
 
         protected override void OnSleep()
         {
-            // Handle when your app sleeps
-            Current.Properties["Alarms"] = Alarm.Alarms;
-            SavePropertiesAsync();
+            //await SaveAlarms();
         }
 
-        protected override void OnResume()
+        protected async override void OnResume()
         {
-            // Handle when your app resumes
-            if (Application.Current.Properties.ContainsKey("Alarms"))
-            {
-                var alarms = Current.Properties["Alarms"] as List<Alarm>;
-                Alarm.Alarms.AddRange(alarms);
-            }
+            await LoadAlarms();
         }
     }
 }
