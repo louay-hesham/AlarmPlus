1d5e6bf7c3f7b3173932d8761bbecdfa7802babf
louay-hesham <louay.hesham@gmail.com>
2017-04-12 15:55:50 +0200

    Backend project added and published

---
+++ AlarmPlus/AlarmPlus.Backend/Startup.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(AlarmPlus.Backend.Startup))]
+
+namespace AlarmPlus.Backend
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureMobileApp(app);
+        }
+    }
+}
