3fb67607fd72d352e7437f2b90a86912c33de617
louay-hesham <louay.hesham@gmail.com>
2017-04-13 17:35:08 +0200

    Extensions and user filters

--- AlarmPlus/AlarmPlus.Backend/DataObjects/Alarm.cs
+++ AlarmPlus/AlarmPlus.Backend/DataObjects/Alarm.cs
@@ -8,6 +8,7 @@ namespace AlarmPlus.Backend.DataObjects
 {
     public class Alarm : EntityData
     {
+        public string UserId { get; set; }
         public int LocalID { get; set; }
         public bool Enabled { get; set; }
         public TimeSpan Time { get; set; }
---
+++ AlarmPlus/AlarmPlus.Backend/Extensions/AlarmExtensions.cs
@@ -0,0 +1,16 @@
+﻿using AlarmPlus.Backend.DataObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AlarmPlus.Backend.Extensions
+{
+    public static class AlarmExtensions
+    {
+        public static IQueryable<Alarm> PerUserFilter(this IQueryable<Alarm> query, string userid)
+        {
+            return query.Where(item => item.UserId.Equals(userid));
+        }
+    }
+}
