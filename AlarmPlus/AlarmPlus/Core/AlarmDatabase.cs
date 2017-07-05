f3bb846cb7542a09143494c55696ad2fa8726ab8
louay-hesham <louay.hesham@gmail.com>
2017-07-05 23:43:31 +0200

    Database

---
+++ AlarmPlus/AlarmPlus/Core/AlarmDatabase.cs
@@ -0,0 +1,54 @@
+﻿using SQLite.Net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AlarmPlus.Core
+{
+    class AlarmDatabase
+    {
+
+        private static SQLiteConnection database;
+
+        public static void InitializeDatabase()
+        {
+            database = new SQLiteConnection(App.DatabasePlatform, DependencyService.Get<IFileHelper>().GetLocalFilePath("AlarmDatabase.db3"));
+            database.CreateTable<Alarm>();
+        }
+
+        public static List<Alarm> GetItems()
+        {
+            return database.Table<Alarm>().ToList();
+        }
+
+        public static List<Alarm> GetItemsNotDone()
+        {
+            return database.Query<Alarm>("SELECT * FROM [TodoItem] WHERE [Done] = 0");
+        }
+
+        public static Alarm GetItem(int id)
+        {
+            return database.Table<Alarm>().Where(i => i.ID == id).FirstOrDefault();
+        }
+
+        public static int SaveItem(Alarm item)
+        {
+            if (item.ID != 0)
+            {
+                return database.Update(item);
+            }
+            else
+            {
+                return database.Insert(item);
+            }
+        }
+
+        public static int DeleteItem(Alarm item)
+        {
+            return database.Delete(item);
+        }
+    }
+}
